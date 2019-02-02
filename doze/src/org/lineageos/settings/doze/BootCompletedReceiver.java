1d356d1f262bf6028f45a3d2d1edb9dbd4316d0b
Harsh Shandilya <msfjarvis@gmail.com>
2019-02-02 20:05:27 +0530

    sdm660-common: parts: Refactor Dirac setup
    - Convert DiracUtils to be an instantiable class.
    
    - Stop tracking initialization state with a boolean and just
      fallback to checking the instance's nullity.
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    Signed-off-by: Anunay Maheshwari <anunaym14@gmail.com>

--- doze/src/org/lineageos/settings/doze/BootCompletedReceiver.java
+++ doze/src/org/lineageos/settings/doze/BootCompletedReceiver.java
@@ -35,7 +35,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
             if (DEBUG) Log.d(TAG, "Starting service");
             Utils.startService(context);
         }
-        DiracUtils.initialize(context);
+        new DiracUtils(context);
     }
-
 }
