4e213ef1364fe385525e126bab6276599a83d7f4
OdSazib <odsazib@gmail.com>
2020-07-16 00:46:32 +0600

    sdm660-common: Add powerhint for sdm660 and sdm636 * Add EAS_POWERHINT_VARIANT := sdm636 for whyred in device.mk
    Co-authored-by: Subhajeet Muhuri <kenny3fcb@gmail.com>
    Signed-off-by: OdSazib <odsazib@gmail.com>
    Change-Id: I3591038a29326b15ce00de0051208219d9f3830d
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -379,6 +379,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
+# Powerhint
+ifeq ($(EAS_POWERHINT_VARIANT), sdm636)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/power-libperfmgr/sdm636_powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+else
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/power-libperfmgr/sdm660_powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+endif
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
