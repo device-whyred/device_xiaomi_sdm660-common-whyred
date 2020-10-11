d3145ce1ee970cd34d1aa06a9c33f783e1dd9c84
Mohammad Iqbal <mi460334@gmail.com>
2020-10-11 04:27:16 +0000

    Revert "stag-ify"
    This reverts commit 6e0c6d232b16ff35f3053f7d93669122b1ed40d7.

--- properties.mk
+++ properties.mk
@@ -1,5 +1,5 @@
 #Build FP to be picked by both system and vendor
-BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys"
+BUILD_FINGERPRINT := "google/crosshatch/crosshatch:10/QQ2A.200501.001.B2/6352890:user/release-keys"
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
