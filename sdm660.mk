175b545cde3b78007e47106f2f0b170e112aa843
mohammad iqbal <mi460334@gmail.com>
2020-11-19 00:12:21 +0000

    Evolver

--- sdm660.mk
+++ sdm660.mk
@@ -28,7 +28,8 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-evo
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
