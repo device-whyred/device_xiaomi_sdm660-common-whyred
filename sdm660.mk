e703f774a54819d658a15442db5bda24cef44fe6
mohammad iqbal <mi460334@gmail.com>
2020-12-03 10:11:20 +0000

    Revert "sdm660-common: Add Moto Dolby"
    This reverts commit 7687d509b3002f845f4102704c06d34f8e848860.

--- sdm660.mk
+++ sdm660.mk
@@ -110,8 +110,6 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     audio.a2dp.default \
     libbthost_if \
-    libldacBT_enc \
-    libldacBT_abr \
     libldacBT_dec \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
@@ -334,7 +332,6 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
