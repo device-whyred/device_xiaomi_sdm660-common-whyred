f33251cdf83bfc9ddb9c7f614b44f7529cdbc965
root <root@iqbal.heymnpl2vh0ujozbrymtoeynvb.cwx.internal.cloudapp.net>
2021-03-30 13:30:03 +0000

    zram

--- properties.mk
+++ properties.mk
@@ -301,6 +301,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Fast App Launch
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.device_config.runtime_native.usap_pool_enabled=true
+
+# ZRAM
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.zram.mark_idle_delay_mins=60 \
     ro.zram.first_wb_delay_mins=180 \
     ro.zram.periodic_wb_delay_hours=24
