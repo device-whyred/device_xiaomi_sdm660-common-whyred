cb03268c2f3e10c61ba51de33a0563c1f5fbf75a
fakeyatogod <fakeyatokami@gmail.com>
2021-04-07 16:00:48 +0530

    sdm660-common: Use Userspace LMKD if there's no LMK driver
    Change-Id: Ib37a9619cbdef18fa6f1abd39ccca0a4b1f768c5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -150,6 +150,7 @@ DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/manifests/compatibility_matrix.xml
 TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_sdm660
 TARGET_RECOVERY_DEVICE_MODULES := libinit_sdm660
 
+<<<<<<< HEAD
 # IPA
 USE_DEVICE_SPECIFIC_DATA_IPA_CFG_MGR := true
 USE_DEVICE_SPECIFIC_IPACFG_MGR := true
@@ -158,6 +159,9 @@ USE_DEVICE_SPECIFIC_IPACFG_MGR := true
 TARGET_PROVIDES_KEYMASTER := true
 
 # LMKD
+=======
+# LMKD stats logging
+>>>>>>> 309e9d543 (sdm660-common: Use Userspace LMKD if there's no LMK driver)
 TARGET_LMKD_STATS_LOG := true
 
 # Partitions
