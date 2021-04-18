b1edf66e5add2434588a1908e2e2bcca01e10e8d
predator112 <mi560334@gmail.com>
2021-04-18 02:09:17 +0000

    fix derp

--- properties.mk
+++ properties.mk
@@ -298,12 +298,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     pm.dexopt.bg-dexopt=speed-profile \
     pm.dexopt.shared=speed
 
-# ZRAM
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.zram.mark_idle_delay_mins=60 \
-    ro.zram.first_wb_delay_mins=180 \
-    ro.zram.periodic_wb_delay_hours=24
-
 # Seamless transfer
 PRODUCT_PROPERTY_OVERRIDES += \
   sys.fflag.override.settings_seamless_transfer=true
