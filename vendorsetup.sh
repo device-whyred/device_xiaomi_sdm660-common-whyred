9027bac52947146007a582f870623959cd869620
predator112 <mi560334@gmail.com>
2021-04-18 04:24:47 +0000

    decomonize

---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+# rm -rf
+rm -rf hardware/qcom-caf/msm8998/display
+rm -rf hardware/qcom-caf/msm8998/audio
+rm -rf hardware/qcom-caf/msm8998/media
+
+# git clone
+git clone https://github.com/AOSPA/android_hardware_qcom_display -b quartz-8998 hardware/qcom-caf/msm8998/display
+git clone https://github.com/AOSPA/android_hardware_qcom_media -b quartz-8998 hardware/qcom-caf/msm8998/media
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-17.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
