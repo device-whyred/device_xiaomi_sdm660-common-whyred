5811d6273f19e9f8579171bbc8478869821bc450
root <root@raphael.ehp0n55wvzhu3m2fz1tduxiosh.lx.internal.cloudapp.net>
2021-06-24 07:46:05 +0000

    p

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+# git clone
+#git clone https://github.com/predator112/STOCK_Q_kernel_whyred -b android-10-eas kernel/xiaomi/sdm660
+#git clone https://github.com/device-whyred/vendor_xiaomi_whyred -b MiuiCamera vendor/MiuiCamera
