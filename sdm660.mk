d83ef22d3b0c8804bfb4ef66a4b89ceffcc861d3
Jeferson <jroliveira.oliveira301@gmail.com>
2022-06-26 03:13:03 +0200

    sdm660-common: Dontaudit gms intrusion
    - ignore this logspam
    
    Change-Id: I5461f6b8048909ac7ea231049f77939ed798264e

--- sdm660.mk
+++ sdm660.mk
@@ -29,11 +29,6 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
-PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
