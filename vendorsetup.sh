f32101579bab1dae6c1a3ab24afaa540f8abc408
VioletBlue90 <VioletBlue90Official@gmail.com>
2022-08-26 15:20:57 +0000

    fix logspam

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
-#rm -rf hardware/qcom-caf/msm8998/display
-#rm -rf hardware/qcom-caf/msm8998/media
-#rm -rf hardware/qcom-caf/msm8998/audio
+rm -rf hardware/qcom-caf/msm8998/display
+rm -rf hardware/qcom-caf/msm8998/media
+rm -rf hardware/qcom-caf/msm8998/audio
 
 # git clone
-git clone https://github.com/predator112/android_kernel_xiaomi_sdm660_southwest -b 11.0-eas-wifi-r kernel/xiaomi/sdm660
-git clone https://github.com/device-whyred/vendor_xiaomi_whyred -b MiuiCamera vendor/MiuiCamera
+#git clone https://github.com/predator112/android_kernel_xiaomi_sdm660_southwest -b 11.0-eas-wifi-r kernel/xiaomi/sdm660
+#git clone https://github.com/device-whyred/vendor_xiaomi_whyred -b MiuiCamera vendor/MiuiCamera
 git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-17.1-caf-msm8998 hardware/qcom-caf/msm8998/display
 git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-17.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
 git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-17.1-caf-msm8998 hardware/qcom-caf/msm8998/media
