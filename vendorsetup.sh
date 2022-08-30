05183dd601a46a6c51a61ca743b10af5e8946498
watchdevil321 <watchdevil321@gmail.com>
2022-08-30 07:47:09 +0000

    nadnii

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@ rm -rf hardware/qcom-caf/msm8998/audio
 # git clone
 #git clone https://github.com/predator112/android_kernel_xiaomi_sdm660_southwest -b 11.0-eas-wifi-r kernel/xiaomi/sdm660
 #git clone https://github.com/device-whyred/vendor_xiaomi_whyred -b MiuiCamera vendor/MiuiCamera
-git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-17.1-caf-msm8998 hardware/qcom-caf/msm8998/display
+git clone https://github.com/NusantaraProject-ROM/android_hardware_qcom_display -b 10-8998-los hardware/qcom-caf/msm8998/display
 git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-17.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
 git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-17.1-caf-msm8998 hardware/qcom-caf/msm8998/media
