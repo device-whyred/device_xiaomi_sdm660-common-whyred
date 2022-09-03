ParsimoniousLavender/device_xiaomi_sdm660-common

sdm660-common: Import AdaptLaunch and some other perf * From LA.UM.8.6.2.r1-08600-89xx.0
Test: Better app launching time

Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
Signed-off-by: clarencelol <clarencekuiek@icloud.com>

sdm660.mk
@@ -91,6 +91,10 @@ PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 endif
 
+# Adapt Launch 
+PRODUCT_COPY_FILES += \
+$(COMMON_PATH)/configs/lm/AdaptLaunchFeature.xml:$(TARGET_COPY_OUT_VENDOR)/etc/lm/AdaptLaunchFeature.xml \
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \

device-whyred/device_xiaomi_sdm660-common-whyred

sdm660.mk
@@ -93,6 +93,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_0_3.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.ethernet.xml
 
+# Adapt Launch 
+PRODUCT_COPY_FILES += \
+ $(LOCAL_PATH)/configs/lm/AdaptLaunchFeature.xml:$(TARGET_COPY_OUT_VENDOR)/etc/lm/AdaptLaunchFeature.xml \
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \